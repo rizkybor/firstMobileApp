4ad0872bc8d1e01ebb017da0ebf2f97940b25acd
Rizky Ajie Kurniawan - Mac <rizkyak994@gmail.com>
2022-11-14 20:18:53 +0700

    firstAdd

--- app/src/main/java/id/tutorials/firstmobileapp/MainActivity.kt
+++ app/src/main/java/id/tutorials/firstmobileapp/MainActivity.kt
@@ -2,12 +2,16 @@ package id.tutorials.firstmobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.myButton)
+        btnClickMe.setOnCLickListener { /*it:view!*/
+            btnClickMe.text = "Haha Click Me"
+        }
     }
-
-
 }
